97707df048bd7400c57dbefd572d9d88e0cc674c
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-03-25 17:30:30 +0900

    20250325 김태영 기본 문제 제출

---
+++ vue/20250325/기본/test-vue-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <!-- <ul>
+      <CheckboxItem
+        v-for="idol in idols"
+        :key="idol.id"
+        :name="idol.name"
+        :checked="idol.checked"
+      />
+    </ul> -->
+    <App2 />
+
+    <App4 />
+  </div>
+</template>
+<script>
+import CheckboxItem from "./components/CheckboxItem.vue";
+import App2 from "./App2.vue";
+import App4 from "./App4.vue";
+
+export default {
+  name: "App",
+
+  components: { CheckboxItem, App2, App4 },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: "BTS", checked: true },
+        { id: 2, name: "Black Pink", checked: false },
+        { id: 3, name: "EXO", checked: false },
+        { id: 4, name: "ITZY", checked: false },
+      ],
+    };
+  },
+};
+</script>
