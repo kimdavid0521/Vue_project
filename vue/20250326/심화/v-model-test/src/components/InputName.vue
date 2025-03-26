4c63133720577fb40051dcf489ba79019b7c02e7
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-03-26 16:30:52 +0900

    20250326 김태영 컴포넌트 심화 심화 문제 제출

---
+++ vue/20250326/심화/v-model-test/src/components/InputName.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :value="name"
+      @input="$emit('update:name', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "inputName",
+  props: ["name"],
+};
+</script>
