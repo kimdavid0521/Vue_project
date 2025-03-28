74161211b9b4ef9138bd3bad74609531a59fb20f
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-03-28 16:36:45 +0900

    20250328 심화문제 김태영 제출

--- vue/20250328/기본과심화/router-test/src/router/index.js
+++ vue/20250328/기본과심화/router-test/src/router/index.js
@@ -1,31 +1,54 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  isNavigationFailure,
+} from "vue-router";
 import Home from "@/pages/Home.vue";
 import About from "@/pages/About.vue";
 import Members from "@/pages/Members.vue";
 import MemberInfo from "@/pages/MemberInfo.vue";
 import Videos from "@/pages/Videos.vue";
+import NotFound from "@/pages/NotFound.vue";
+
+const membersIdGuard = (to, from) => {
+  if (from.name !== "members" && from.name !== "members/id") {
+    return false;
+  }
+};
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "home",
       component: Home,
     },
     {
       path: "/about",
+      name: "about",
       component: About,
     },
     {
       path: "/members",
+      name: "members",
       component: Members,
     },
     {
       path: "/members/:id",
+      name: "members/id",
       component: MemberInfo,
+      beforeEnter: membersIdGuard,
     },
     {
       path: "/videos",
+      name: "videos",
       component: Videos,
+    },
+    {
+      path: "/:paths(.*)*",
+      name: "NotFound",
+      component: NotFound,
     },
   ],
 });
